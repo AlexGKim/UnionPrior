5d8fe0581c2719e25e8534469b67b61983dda44e
Alex <agkim@lbl.gov>
2024-09-23 16:58:14 +0900

    Update before going home

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def node_to_cosmo():
 
 	w0_pdf = uniform(-1,1)
 	wa_pdf = uniform(-0.2,0.4)
-	Om0_pdf = uniform(cosmo_0.Om0-.1, cosmo_0.Om0+.1)
+	Om0_pdf = uniform(cosmo_0.Om0-.02, cosmo_0.Om0+.02)
 
 	zs = numpy.concatenate([numpy.arange(0.05,0.8,0.05),numpy.arange(0.8,1.21,0.1)])
 
@@ -36,7 +36,7 @@ def node_to_cosmo():
 	fig.show()
 
 def cosmo_to_node():
-	n = 1000
+	n = 100
 
 	cosmo_0 = FlatLambdaCDM(H0=70, Om0=0.3)
 
@@ -48,21 +48,27 @@ def cosmo_to_node():
 		ans = ((mu - _cosmo.distmod(zs).value)**2).sum()
 		return ans
 
+	index=0
 	results=[]
-	for _ in range(n):
-		mu = mu0 + uniform.rvs(loc=-.05,scale=.1,size=len(zs))
+	while (index<n):
+		mu = mu0 + uniform.rvs(loc=-.1,scale=.2,size=len(zs))
 		res = minimize(f,(0.3,-1, 0, 70),args=mu,bounds=((0,1),(-5,1),(-10,10),(50,100)))
-		if not res.success:
-			raise Exception("Optimization Failed")
-		results.append(res.x)
+		if res.success and res.fun<0.04:
+			print(res.fun)
+			results.append(res.x)
+			index = index+1
 
-	ubf = {'$\Omega_M$':0.244, '$w_0$':-.735, '$w_a$':0  } 
 
 	df = pandas.DataFrame(results,columns=[r'$\Omega_M$', r'$w_0$', r'$w_a$', r'$H_0$'])
 	# df.to_csv("cosmo_to_model.tmp.csv")
 	c = ChainConsumer()
 	c.add_chain(Chain(samples=df, name="Prior"))
+	ubf = {'$\Omega_M$':0.244, '$w_0$':-.735, '$w_a$':0  } 
+	sc = {'$w_0$':-1, '$w_a$':0  } 
+
 	c.add_marker(location=ubf,name="Union3 FlatwCDM",color='red',marker_size="40.")
+	c.add_marker(location=sc,name="LCDM",color='green',marker_size="40.")
+
 	fig = c.plotter.plot(figsize=10)
 	fig.show()
 
