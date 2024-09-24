7e35930c0268d2af33569be3fb72c1d5375cee48
Alex <agkim@lbl.gov>
2024-09-24 17:53:27 +0900

    start implementation with jacobian

--- main.py
+++ main.py
@@ -1,37 +1,129 @@
 from astropy.cosmology import FlatLambdaCDM
 from astropy.cosmology import Flatw0waCDM
+from astropy.cosmology import Planck15
 from scipy.optimize import minimize
 
 from scipy.stats import uniform
 import numpy
 from chainconsumer import Chain, ChainConsumer, make_sample
 import pandas
+import matplotlib.pyplot as plt
+
+import jax
+import jax_cosmo as jc
+import jax.numpy as jnp
+import astropy
+
+
+def pdf():
+	zs = numpy.concatenate([numpy.arange(0.05,0.8,0.05),numpy.arange(0.8,1.21,0.1)])
+	aas = 1/(1+zs)
+	w0s = numpy.linspace(-1-2*0.11, 4*0.11,10)
+	was =  numpy.linspace(0-2*0.37, 4*0.37,10)
+	Om0s = numpy.linspace(0.3-.05, 0.3+.1,10)
+
+
+	cosmo_0 = jc.Planck15(Omega_c = 0.3, w0=1., wa=0.)
+	dL = (1+zs) * jc.background.transverse_comoving_distance(cosmo_0 ,aas) # In [Mpc/h]
+	mu_0 = 5*jnp.log10(dL) + 25
+
+	def nodes(W):
+		cosmo = jc.Planck15(Omega_c = W[0]-0.0486, w0=W[1], wa=W[2])
+		dL = (1+zs) * jc.background.transverse_comoving_distance(cosmo ,aas) # In [Mpc/h]
+		mu = 5*jnp.log10(dL) + 25
+		nodes = mu-mu_0
+		return nodes
+
+	J_nodes = jax.jacfwd(nodes)
+
+	ans=[]
+	for Om0 in Om0s:
+		for w0 in w0s:
+			for wa in was:
+				W = jnp.array((Om0, w0, wa))
+				J = J_nodes(W)
+				print(jnp.dot(J.T,J))
+
 
 def node_to_cosmo():
-	n = 5000	
+	cosmo_desi = Flatw0waCDM(H0=70, Om0=0.343, w0 = -0.64, wa = -1.27)
 	cosmo_0 = FlatLambdaCDM(H0=70, Om0=0.3)
 
-	w0_pdf = uniform(-1,1)
-	wa_pdf = uniform(-0.2,0.4)
-	Om0_pdf = uniform(cosmo_0.Om0-.02, cosmo_0.Om0+.02)
+	n = 10000
+
+	w0_pdf = uniform(cosmo_desi.w0-2*0.11, 4*0.11)
+	wa_pdf = uniform(cosmo_desi.wa-2*0.37, 4*0.37)
+	Om0_pdf = uniform(cosmo_0.Om0-.05, cosmo_0.Om0+.1)
 
 	zs = numpy.concatenate([numpy.arange(0.05,0.8,0.05),numpy.arange(0.8,1.21,0.1)])
 
-
 	mu0=cosmo_0.distmod(zs)
 
-	residuals = []
+	residuals_Om0 = []
+	residuals_w0 = []
+	residuals_wa = []
 
 	for _ in range(n):
-		_cosmo = Flatw0waCDM(H0=70, Om0=Om0_pdf.rvs(), w0 = w0_pdf.rvs(),wa = wa_pdf.rvs()) 
+		_cosmo = Flatw0waCDM(H0=70, Om0=Om0_pdf.rvs(), w0 = -1,wa = 0)
 		mu = _cosmo.distmod(zs)
-		residuals.append((mu - mu0).value)
+		residuals_Om0.append((mu - mu0).value)
+
+		_cosmo = Flatw0waCDM(H0=70, Om0=cosmo_0.Om0, w0 = w0_pdf.rvs(), wa = 0)
+		mu = _cosmo.distmod(zs)
+		residuals_w0.append((mu - mu0).value)
+
+		_cosmo = Flatw0waCDM(H0=70, Om0=cosmo_0.Om0, w0 = -1, wa = wa_pdf.rvs())
+		mu = _cosmo.distmod(zs)
+		residuals_wa.append((mu - mu0).value)	
+
+	residuals_Om0 = numpy.array(residuals_Om0)
+	residuals_w0 = numpy.array(residuals_w0)
+	residuals_wa = numpy.array(residuals_wa)
 
 	cols = ["N. {}".format(i) for i in range(len(zs))]
-	df = pandas.DataFrame(residuals,columns=cols)
+	df_Om0 = pandas.DataFrame(residuals_Om0,columns=cols)
+	df_w0 = pandas.DataFrame(residuals_w0,columns=cols)
+	df_wa = pandas.DataFrame(residuals_wa,columns=cols)
+
+	fig, axs = plt.subplots(4,5, figsize=(12, 8), layout='constrained')
+	for index, ax in enumerate(axs.flat):
+	    ax.set_title("Index {}".format(index))
+	    ax.hist(
+	    	[df_Om0.iloc[:,index],df_w0.iloc[:,index],df_wa.iloc[:,index]], 20,
+	    	label=[r"$\Omega_M$",r"$w_0$",r"$w_a$"]
+	    	)
+	    # ax.hist(df_w0.iloc[:,index],label=r"$w_0$")
+	    # ax.hist(df_wa.iloc[:,index],label=r"$w_a$")
+	    if index==0:
+	    	ax.legend()
+
+	fig.show()
+
+	fig, axs = plt.subplots(4,5, figsize=(12, 8), layout='constrained')
+	for index, ax in enumerate(axs.flat):
+	    ax.set_title("Index {}".format(index))
+	    ax.hist(
+	    	[df_w0.iloc[:,index],df_wa.iloc[:,index]], 20,
+	    	label=[r"$w_0$",r"$w_a$"]
+	    	)
+	    if index==0:
+	    	ax.legend()
+
+	fig.show()
+
+	fig, axs = plt.subplots(4,5, figsize=(12, 8), layout='constrained')
+	for index, ax in enumerate(axs.flat):
+	    ax.set_title("Index {}".format(index))
+	    ax.hist(df_w0.iloc[:,index],label=r"$w_0$")
+
+	fig.show()
+
 
 	c = ChainConsumer()
-	c.add_chain(Chain(samples=df, name="Prior"))
+	# c.add_chain(Chain(samples=df_Om0, name=r"$\Omega_M$"))
+	c.add_chain(Chain(samples=df_w0, name=r"$w_0$"))
+	c.add_chain(Chain(samples=df_wa, name=r"$w_a$"))
+
 	fig = c.plotter.plot(figsize=10)
 	fig.show()
 
