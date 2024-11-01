d0c0d20c3c11d0339adda6505020ea300e605fdf
Alex <agkim@lbl.gov>
2024-11-01 01:57:13 -0700

    new version after Pat discussion

--- main.py
+++ main.py
@@ -18,6 +18,120 @@ from scipy.stats import chi2
 
 jax.config.update("jax_enable_x64", True)
 
+def posterior():
+	f1="mu_mat_union3_cosmo=2.fits"
+	hdu_list = fits.open(f1, memmap=True)
+	invcov = hdu_list[0].data
+	invcov = invcov.astype("float")
+	zs = invcov[0,1:]
+	n0 = invcov[1:,0]
+	invcov = invcov[1:,1:]
+
+	desi = [-0.64,-1.27]
+
+	aas = 1/(1+zs)
+
+	# w0s = numpy.linspace(-1.2,-0.2,3)
+	# was =  numpy.linspace(-2, 2 ,3)
+	w0s = numpy.linspace(-1.1,-0.3,20)
+	was =  numpy.linspace(-7, 0, 20)	
+	# was =  numpy.linspace(-0, 1, 20)	
+
+	# Om0s = numpy.linspace(0.3-.05+0.025, 0.3+.05+0.025,9)
+	Om0s = numpy.linspace(0.3, 0.5,9)
+
+	cosmo_0 = jc.Planck15(Omega_c = 0.3, Omega_b=0, w0=-1., wa=0.)
+	dL = (1+zs) * jc.background.transverse_comoving_distance(cosmo_0 ,aas) # In [Mpc/h]
+	mu_0 = 5*jnp.log10(dL) + 25
+
+	def nodes(W):
+		cosmo = jc.Planck15(Omega_c = W[0],Omega_b=0,  w0=W[1], wa=W[2])
+		dL = (1+zs) * jc.background.transverse_comoving_distance(cosmo ,aas) # In [Mpc/h]
+		mu = 5*jnp.log10(dL) + 25
+		nodes = mu-mu_0
+		return nodes
+
+	if True:
+		lnp_union = numpy.load("lnp_union_posterior.npy")
+		likelihood_union = numpy.load("likelihood_posterior.npy")		
+	else:
+		likelihood_union=numpy.zeros((len(Om0s),len(was),len(w0s)))
+		lnp_union = numpy.zeros((len(Om0s),len(was),len(w0s))) # row - column
+		for i, Om0 in enumerate(Om0s):
+			for k, wa in enumerate(was):
+				for j, w0 in enumerate(w0s):
+					W = jnp.array((Om0, w0, wa))
+					N = nodes(W)
+
+					lnp_union[i,k,j] = -0.5 * ((N.T-n0) @ invcov @ (N-n0))
+					likelihood_union[i,k,j] = lnp_union[i,k,j] + 0.5 * (N**2).sum()
+
+		numpy.save("lnp_union_posterior",lnp_union)
+		numpy.save("likelihood_posterior",likelihood_union)
+
+
+	X, Y = numpy.meshgrid(w0s, was)
+	max_value = lnp_union.max()
+	local_max_index = numpy.where(lnp_union==max_value)
+
+
+	levels = -chi2.isf([1-0.9545, 1-0.9167, 1-0.8427, 1-.6827],22)/2
+
+
+	max_value2 = likelihood_union.max()
+	local_max_index2 = numpy.where(likelihood_union==max_value2)
+
+	levels = -chi2.isf([1-0.9545, 1-0.9167, 1-0.8427, 1-.6827],22)/2
+	levels2= levels
+
+	fig, axs = plt.subplots(3,3, figsize=(12,12))
+	for Om0s_index, ax in enumerate(axs.flat):
+		# levels=zero_level+lnp_union[Om0s_index,:,:].max()
+		CS = ax.contour(X, Y, lnp_union[Om0s_index,:,:], levels=levels, colors='red')
+
+		ax.clabel(CS, CS.levels, inline=True, fontsize=8)
+		# _holder = lnp_union[Om0s_index,:,:]+ logomega[Om0s_index,:,:]
+		# levels=zero_level+_holder.max()
+		CS2 = ax.contour(X, Y, likelihood_union[Om0s_index,:,:], levels=levels2,colors='blue')
+		ax.clabel(CS2, CS2.levels,  inline=True, fontsize=8)
+
+
+		# if local_max_index[0] == Om0s_index:
+		# 	max_x = X[local_max_index[1], local_max_index[2]]
+		# 	max_y = Y[local_max_index[1], local_max_index[2]]		
+		# 	ax.scatter(max_x, max_y ,marker='*',color='red',s=64,label=r'max $\ln{p_U}$')
+
+
+		# if local_max_index2[0] == Om0s_index:
+		# 	max_x = X[local_max_index2[1], local_max_index2[2]]
+		# 	max_y = Y[local_max_index2[1], local_max_index2[2]]			
+		# 	ax.scatter(max_x, max_y ,marker='*',color='blue',s=64,label=r'max $\ln{p_F}$')	
+
+
+
+
+		# ax.set_xlabel(r"$w_0$")
+		# ax.set_ylabel(r"$w_a$")
+		# ax.scatter(desi[0],desi[1],label="DESI Best Fit", color='green')
+		# ax.scatter(-1,0,label=r"$\Lambda$CDM",color='brown')
+		# if Om0s_index<6:
+		# 	legend_loc=3
+		# else:
+		# 	legend_loc= 1
+		# levels=z
+		# ax.legend(loc=legend_loc)
+		ax.set_title(r"$\Omega_M={:7.4f}$".format(Om0s[Om0s_index]))
+
+	fig.suptitle(r"$\ln{p}_U$ (red); $\ln{p}_U-\ln{p^\text{Union3}_\text{prior}}$ (blue)")
+	fig.tight_layout()
+	# fig.show()
+
+	fig.savefig('posterior.pdf')
+	fig.savefig('posterior.png')
+
+posterior()
+wef
+
 def pdf():
 
 	# f2="mu_mat_union3_cosmo=2_mu.fits"
@@ -55,8 +169,9 @@ def pdf():
 		return nodes
 
 	J_nodes = jax.jacfwd(nodes)
+	dfdtheta = jax.grad(nodes)
 
-	if True:
+	if False:
 		lnp_2 = numpy.load("lnp_2.npy")	
 		lnp_union = numpy.load("lnp_union.npy")
 		logomega = numpy.load("logomega.npy")		
@@ -69,6 +184,8 @@ def pdf():
 					W = jnp.array((Om0, w0, wa))
 					N = nodes(W)
 					J = J_nodes(W)
+					dfdt = numpy.abs(dfdt)
+
 					logomega[i,k,j] = -0.5 * (N**2).sum() + 0.5 * jnp.log(jnp.linalg.det(jnp.dot(J.T,J)))
 					lnp_union[i,k,j] = -0.5 * ((N.T-n0) @ invcov @ (N-n0))
 
@@ -150,6 +267,66 @@ def pdf():
 	fig.savefig('contour.png')
 	# fig.show()
 
+	max_value2 = lnp_2.max()
+	print("lnp 2 max ",max_value2)
+	local_max_index2 = numpy.where(lnp_2==max_value2)
+
+	# levels2 = zero_level*one_68_full/2 #+ max_value2
+	levels2= levels
+
+	fig, axs = plt.subplots(3,3, figsize=(12,12))
+	for Om0s_index, ax in enumerate(axs.flat):
+		# levels=zero_level+lnp_union[Om0s_index,:,:].max()
+		CS = ax.contour(X, Y, lnp_union[Om0s_index,:,:], levels=levels, colors='red')
+
+		ax.clabel(CS, CS.levels, inline=True, fontsize=8)
+		# _holder = lnp_union[Om0s_index,:,:]+ logomega[Om0s_index,:,:]
+		# levels=zero_level+_holder.max()
+		CS2 = ax.contour(X, Y, lnp_2[Om0s_index,:,:], levels=levels2,colors='blue')
+		ax.clabel(CS2, CS2.levels,  inline=True, fontsize=8)
+
+		# max_value = lnp_union[Om0s_index,:,:].max()
+		# get position index of this calue in your data array 
+		# local_max_index = numpy.where(lnp_union[Om0s_index,:,:]==max_value)
+	    ## retrieve position of your
+		# max_x = X[local_max_index[0], local_max_index[1]]
+		# max_y = Y[local_max_index[0], local_max_index[1]]
+		if local_max_index[0] == Om0s_index:
+			max_x = X[local_max_index[1], local_max_index[2]]
+			max_y = Y[local_max_index[1], local_max_index[2]]		
+			ax.scatter(max_x, max_y ,marker='*',color='red',s=64,label=r'max $\ln{p_U}$')
+
+		# max_value = _holder.max()
+		# get position index of this calue in your data array 
+		# local_max_index = numpy.where(_holder==max_value)
+	    ## retrieve position of your
+		# max_x = X[local_max_index[0], local_max_index[1]]
+		# max_y = Y[local_max_index[0], local_max_index[1]]
+		if local_max_index2[0] == Om0s_index:
+			max_x = X[local_max_index2[1], local_max_index2[2]]
+			max_y = Y[local_max_index2[1], local_max_index2[2]]			
+			ax.scatter(max_x, max_y ,marker='*',color='blue',s=64,label=r'max $\ln{p_F}$')	
+
+
+		ax.set_xlabel(r"$w_0$")
+		ax.set_ylabel(r"$w_a$")
+		ax.scatter(desi[0],desi[1],label="DESI Best Fit", color='green')
+		ax.scatter(-1,0,label=r"$\Lambda$CDM",color='brown')
+		if Om0s_index<6:
+			legend_loc=3
+		else:
+			legend_loc= 1
+		# levels=z
+		ax.legend(loc=legend_loc)
+		ax.set_title(r"$\Omega_M={:7.4f}$".format(Om0s[Om0s_index]))
+
+	fig.suptitle(r"$\ln{p}_U$ (red); $\ln{p}_F$ (blue)")
+	fig.tight_layout()
+	# fig.show()
+
+	fig.savefig('posterior.pdf')
+	fig.savefig('posterior.png')
+	# fig.show()
 
 
 	fig, axs = plt.subplots(3,3, figsize=(12,12))
